f8a1c4f6b25bf47741583e7fd3f61d3d5ac91274
LNJORA <lnjora.w@gmail.com>
2023-07-10 13:14:40 +0300

    initial commit

---
+++ djangobootstrap_lq/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def post(request):
+    return render(request, "post.html")
